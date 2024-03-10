076dd5c897d2952a78beaf4bd71a4f568bd4c9bf
sefyou36 <rezoukiy@gmail.com>
2024-03-10 19:00:53 +0000

    simulateur de Crédit

--- main.c
+++ main.c
@@ -1,70 +1,29 @@
 #include <stdio.h>
 
-struct Article {
-    char nom[20];
-    float prix;
-    int quantite;
-};
-
-float total(struct Article article) {
-    return article.prix * article.quantite;
-}
-
-float calculerTaxe(struct Article articles[3], float tauxTVA) {
-    float totalTaxe = 0;
-    for (int i = 0; i < 3; i++) {
-        totalTaxe += tauxTVA * total(articles[i]);
-    }
-    return totalTaxe;
-}
-
-void fonction3(float ht, float tva){
-    float ttc;
-    ttc= ht+ (ht*tva)/100;
-
-}
-
 int main() {
+    int tentative = 0;
+    double pret, taux, tauxMensuel, dure, paiement;
 
-    char client[20];
-    char paiement[20];
+    while (tentative < 10) {
+        tentative++;
 
+        printf("Saisissez le montant du pret : ");
+        scanf("%lf", &pret);
 
-    printf("Entrer le nom du client: ");
-    scanf("%s", client);
+        printf("Saisissez le taux d'intérêt (en pourcentage) : ");
+        scanf("%lf", &taux);
+        tauxMensuel = taux / 100 / 12;
 
-    printf("Entrer le mode de paiement: ");
-    scanf("%s", paiement);
+        printf("Indiquez la dure du pret (en années) : ");
+        scanf("%lf", &dure);
 
-    struct Article articles[3];
-    for (int i = 0; i < 3; i++) {
-        printf("\nEntrer les details pour l'article %d:\n", i+1);
-        printf("Nom: ");
-        scanf("%s", articles[i].nom);
-        printf("Prix: ");
-        scanf("%f", &articles[i].prix);
-        printf("Quantite: ");
-        scanf("%d", &articles[i].quantite);
-        fonction3(articles[i].prix, 0.20);
+        if (dure < 5) {
+            printf("La dure du pret doit être d'au moins 5 ans.\n");
+            continue;
+        }
+        paiement = (pret * tauxMensuel) / (1 - pow(1 + tauxMensuel, -dure * 12));
+        printf("La mensualité estimée est de : %.2lf\n\n", paiement);
     }
-
-    float totalHT = 0;
-    for (int i = 0; i < 3; i++) {
-        totalHT += total(articles[i]);
-    }
-    float taxe = calculerTaxe(articles, 0.20);
-    float totalTTC = totalHT + taxe;
-
-    printf("\n\n**** FACTURE ****\n");
-    printf("Client: %s\n", client);
-    printf("Payment Mode: %s\n", paiement);
-    printf("\nArticles:\n");
-    for (int i = 0; i < 3; i++) {
-        printf("\n%s - %d x %.2f = %.2f", articles[i].nom, articles[i].quantite, articles[i].prix, total(articles[i]));
-    }
-    printf("\n\nTotal HT: %.2f", totalHT);
-    printf("\nTVA 20 % : %.2f", taxe);
-    printf("\nTotal TTC: %.2f", totalTTC);
 
     return 0;
 }
